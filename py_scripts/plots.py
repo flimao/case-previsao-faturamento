c5e71d0c8108d14ecc887eac7431907172b61ee6
Felipe Oliveira <flimao@petrobras.com.br>
2021-11-16 21:31:42 -0300

    Atualizar pacote com funções úteis.

--- py_scripts/plots.py
+++ py_scripts/plots.py
@@ -3,6 +3,7 @@ from typing import List
 import pandas as pd
 import matplotlib.pyplot as plt
 import seaborn as sns
+from statsmodels.tsa.seasonal import seasonal_decompose
 
 def dados_faltantes(tswide: pd.DataFrame, produtos: List = None) -> None:
     """Function to greet
@@ -39,3 +40,29 @@ def dados_faltantes(tswide: pd.DataFrame, produtos: List = None) -> None:
     fig.suptitle('Dados faltantes', color = 'red', fontweight = 1000)
     plt.tight_layout()
     plt.show()
+
+def decomp_fourier(serie_fat: pd.Series, produto: str, c: str) -> object:
+    decomp = seasonal_decompose(serie_fat)
+
+    fig, axs = plt.subplots(nrows = 4, figsize = (10, 8), sharex = True)
+
+    ts_filtro = serie_fat
+
+    sns.lineplot(data = ts_filtro, ax = axs[0], color = c)
+    axs[0].set_title('Serie')
+    axs[0].set_ylabel('Faturamento (R$ bi)')
+
+    sns.lineplot(data = decomp.trend, ax = axs[1], color = c)
+    axs[1].set_ylabel('R$ 100 mi')
+
+    sns.lineplot(data = decomp.seasonal, ax = axs[2], color = c)
+    axs[2].set_ylabel('R$ mi')
+
+    resid_standard = (decomp.resid - decomp.resid.mean()) / decomp.resid.std()
+    sns.scatterplot(data = resid_standard, ax = axs[3], color = c)
+    axs[3].set_ylabel('Resíduos padrão')
+
+    fig.suptitle(f"Decomposicao temporal: produto '{produto}'")
+    plt.show()
+
+    return decomp
